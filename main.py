06380a1d4ab7f4207e6ab78d5c8a2ddc9a01d23c
Daniel <dwsuarez98@gmail.com>
2024-05-02 18:30:25 -0300

    modulos: banner

--- main.py
+++ main.py
@@ -6,16 +6,7 @@ una traducción propia dada la literalidad de la librería de Google, que se
 presta a interpretaciónes erróneas.
 """
 import os
-
-banner = """
- _____      _     _                       _       _
-/  ___|    | |   | |                     | |     | |
-\ `--. _ __| |_  | |_ _ __ __ _ _ __  ___| | __ _| |_ ___  _ __
- `--. \ '__| __| | __| '__/ _` | '_ \/ __| |/ _` | __/ _ \| '__|
-/\__/ / |  | |_  | |_| | | (_| | | | \__ \ | (_| | || (_) | |
-\____/|_|   \__|  \__|_|  \__,_|_| |_|___/_|\__,_|\__\___/|_|
-
-"""
+from banner import banner
 
 
 
@@ -33,7 +24,6 @@ def scan_dir():
     for elemento in contenido:
         if elemento.endswith(".srt"):
             print(elemento)
-
 
 
 if __name__ == '__main__':
