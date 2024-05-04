13a9283d7c0cbea6a095b37a73d342ed1d49cb3d
Daniel <dwsuarez98@gmail.com>
2024-05-04 15:27:35 -0300

    lograda, la traducción, pero no el subtutlo

--- main.py
+++ main.py
@@ -6,17 +6,11 @@ una traducción propia dada la literalidad de la librería de Google, que se
 presta a interpretaciónes erróneas.
 """
 import os
+from re import sub
 from banner import banner
 import pysrt
+from googletrans import Translator
 
-
-def menu():
-    print(f"""
-          {banner}
-    opciones:
-
-    1) - Scanear directorios en busca de archivos .srt
-    """)
 
 def scan_dir():
     directorio_actual = os.getcwd()
@@ -28,14 +22,40 @@ def scan_dir():
             nombre_subtitulo = elemento
 
 def read_file():
+    scan_dir()
+    global subs
+    global subtitulo_original
+
     subs = pysrt.open(nombre_subtitulo)
-    subtitulo = subs[2]
-    print(subtitulo)
+    subtitulo_original = subs
 
+def translate_sub( ):
+    t = Translator()
+    count = 0
+    subs_nuevos = pysrt.open("Whiplash.srt")
+    for linea in range(len(subtitulo_original)):
+        linea_actual = subtitulo_original[linea].text
 
+        try:
+            subs_nuevos[linea].text = t.translate(linea_actual, dest='es')
+            count += 1
+            print(count)
+        except:
+            pass
+
+    subs_nuevos.save("nuevo_subtitulo.srt")
+
+def menu():
+    print(f"""
+          {banner}
+    opciones:
+
+    1) - Scanear directorios en busca de archivos .srt
+    """)
 
 
 if __name__ == '__main__':
-    menu()
+    #menu()
     scan_dir()
     read_file()
+    translate_sub()
