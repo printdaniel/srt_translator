768efd98e087722532b6fc42d2c1be8fec609169
Daniel <dwsuarez98@gmail.com>
2024-05-06 20:02:04 -0300

    fix: retroceso

--- main.py
+++ main.py
@@ -13,36 +13,34 @@ from googletrans import Translator
 
 
 def scan_dir():
-    directorio_actual = os.getcwd()
-    contenido = os.listdir(directorio_actual)
+    contenido = os.listdir(os.getcwd())
     global nombre_subtitulo
     for elemento in contenido:
         if elemento.endswith(".srt"):
-            print(elemento)
             nombre_subtitulo = elemento
+            return nombre_subtitulo
+    return None
 
 def read_file():
     scan_dir()
-    global subs
     global subs_originales
 
     subs = pysrt.open(nombre_subtitulo, encoding='latin-1')
     subs_originales = subs
 
 def translate_sub():
-    t = Translator()
+    translator = Translator()
     subs_nuevos = pysrt.SubRipFile()
-    print("traduccion iniciado")
 
     for subtitulo in subs_originales:
         try:
-            texto_traducido = t.translate(subtitulo.text, dest="es").text
+            texto_traducido = translator.translate(subtitulo.text, dest="es").text
             subs_nuevos.append(pysrt.SubRipItem(start=subtitulo.start, end=subtitulo.end, text=texto_traducido))
         except Exception as e:
             print(f"Error al traducir subtítulo: {e}")
 
-    subs_nuevos.save("nuevo_subtitulo.srt")
-    print("Traducción completada. Se ha guardado el archivo 'nuevo_subtitulo.srt'.")
+    subs_nuevos.save("subtitulo_traducido.srt")
+    print("Traducción completada. Se ha guardado el archivo 'subtitulo_traducido.srt'.")
 
 def menu():
     print(f"""
@@ -52,7 +50,8 @@ def menu():
     1) - Scanear directorios en busca de archivos .srt
     """)
 
+
+# Ejemplo de uso
 if __name__ == '__main__':
     menu()
     read_file()
-    translate_sub()
