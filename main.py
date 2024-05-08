d815c57cadb852547c73262228450db09c8be3ee
Daniel <dwsuarez98@gmail.com>
2024-05-08 14:37:56 -0300

    refact: refinado de fuciones, revisar si modularizar mejor

--- main.py
+++ main.py
@@ -11,36 +11,51 @@ from assets.lenguages import LANGUAGES
 import pysrt
 from googletrans import Translator
 
+def scan_directory_for_subtitle():
+    """
+    Escanea el directorio especificado en busca de archivos .srt y devuelve el
+    nombre del primer archivo encontrado.
 
-def scan_dir():
-    contenido = os.listdir(os.getcwd())
-    global nombre_subtitulo
-    for elemento in contenido:
-        if elemento.endswith(".srt"):
-            nombre_subtitulo = elemento
-            return nombre_subtitulo
+    Parameters:
+    - directory: El directorio a escanear.
+
+    Returns:
+    - El nombre del archivo de subtítulo encontrado, o None si no se
+    encontraron archivos de subtítulos.
+    """
+    for filename in os.listdir(os.getcwd()):
+        if filename.endswith(".srt"):
+            return filename
     return None
 
 def read_file():
-    scan_dir()
-    global subs_originales
+    filename = scan_directory_for_subtitle()
 
-    subs = pysrt.open(nombre_subtitulo, encoding='latin-1')
-    subs_originales = subs
+    if filename:
+        subs = pysrt.open(filename, encoding='latin-1')
+        subs_originales = subs
+        return subs_originales
+    else:
+        print("No se encontró ningún archivo de subtitulo en el directorio")
+        return None
 
 def translate_sub():
     translator = Translator()
+    subs_originales = read_file()
     subs_nuevos = pysrt.SubRipFile()
 
-    for subtitulo in subs_originales:
-        try:
-            texto_traducido = translator.translate(subtitulo.text, dest="es").text
-            subs_nuevos.append(pysrt.SubRipItem(start=subtitulo.start, end=subtitulo.end, text=texto_traducido))
-        except Exception as e:
-            print(f"Error al traducir subtítulo: {e}")
+    if subs_originales:
+        for subtitulo in subs_originales:
+            try:
+                texto_traducido = translator.translate(subtitulo.text, dest="es").text
+                subs_nuevos.append(pysrt.SubRipItem(start=subtitulo.start, end=subtitulo.end, text=texto_traducido))
+            except Exception as e:
+                print(f"Error al traducir subtítulo: {e}")
 
-    subs_nuevos.save("subtitulo_traducido.srt")
-    print("Traducción completada. Se ha guardado el archivo 'subtitulo_traducido.srt'.")
+        subs_nuevos.save("subtitulo_traducido.srt")
+        print("Traducción completada. Se ha guardado el archivo 'subtitulo_traducido.srt'.")
+    else:
+        ("No se pudo cargar el archivo de subtitulos para la traducción")
 
 def menu():
     print(f"""
@@ -51,7 +66,6 @@ def menu():
     """)
 
 
-# Ejemplo de uso
 if __name__ == '__main__':
-    menu()
-    read_file()
+    translate_sub()
+
